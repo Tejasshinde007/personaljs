db55c9beaf038546ca56fbffb6e6b940f4b58264
Tejasshinde007 <tejasshinde20033@gmail.com>
2025-02-07 19:47:49 +0530

    personal js

---
+++ if_else.js
@@ -0,0 +1,24 @@
+// Syntax:
+// if (expression){
+//     // code
+// }
+// else{
+//     // code
+// }
+
+// Example
+
+// var a=20
+// if (a%2==0){
+//     document.writeln(a,"is even number")
+// }
+// else{
+//     document.writeln(a,"not a even number")
+// }
+
+// Diaglog box
+
+
+
+
+
---
+++ intro.js
@@ -0,0 +1,15 @@
+// It is used to display the msg on the html page
+document.writeln("Introduction To Js")
+
+// it is used to debugging the code
+console.log("Hello World!");
+
+var name="Tejas"
+// name="Shinde"  // here we are reassing the value
+// name=false
+// name=1.24
+console.log(name);
+document.writeln("<br>")  // we can also use html tage as well
+document.writeln("<h1>"+name+"</h1>")  //here we have use html tag 
+
+
