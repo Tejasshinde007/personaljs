6f9e36fb54a7d151c8c2f2263f7a45fc9037bc82
Tejasshinde007 <tejasshinde20033@gmail.com>
2025-02-13 18:46:12 +0530

    second

---
+++ inbuiltobject.js
@@ -0,0 +1,19 @@
+// In built objects in js
+
+// 1)math
+
+// console.log(Math.random());
+// console.log(Math.max(1,3,2,4,5,4,6));
+// console.log(Math.round(1.2));
+// for all the methods in math refer the mdn website
+
+
+// 2)String
+
+// let lastName="Shinde"
+// console.log(typeof(lastName));  // it will show string bcz it primitive type
+
+// but we want object in js ther are to strings one in primitive and second one is object
+
+let firstName= new String("Tejas")
+console.log(typeof(firstName));
